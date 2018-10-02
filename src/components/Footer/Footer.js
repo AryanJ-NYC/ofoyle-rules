1e924ba7cbf4e9f7fec2fd309e5642b565aef3a7
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2018-10-01 22:46:04 -0400

    Fix form

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,50 +1,99 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Button, Col, Container, Form } from 'react-bootstrap';
 import styles from './footer.module.css';
 
-const Footer = () => (
-  <footer>
-    <Form name="contact" method="POST" netlify="true">
-      <Container className={styles.form}>
-        <h1 className={styles.header}>Contact Us</h1>
-          <Form.Row>
-            <Col sm={12} md={6}>
-              <Form.Group controlId="firstName">
-                <Form.Label>First Name</Form.Label>
-                <Form.Control type="text" name="first-name"/>
-              </Form.Group>
-            </Col>
-            <Col sm={12} md={6}>
-              <Form.Group controlId="lastName">
-                <Form.Label>Last Name</Form.Label>
-                <Form.Control type="text" name="last-name" />
-              </Form.Group>
-            </Col>
-            <Col sm={12} md={6}>
-              <Form.Group controlId="emailAddress">
-                <Form.Label>Email Address</Form.Label>
-                <Form.Control type="email" name="email-address" />
-              </Form.Group>
-            </Col>
-            <Col sm={12} md={6}>
-              <Form.Group controlId="phoneNumber">
-                <Form.Label>Phone Number</Form.Label>
-                <Form.Control type="tel" name="telephone-number" />
-              </Form.Group>
-            </Col>
-            <Col sm={12} md={6}>
-              <Form.Group controlId="Inquiry">
-                <Form.Label>Inquiry</Form.Label>
-                <Form.Control as="textarea" rows="5" name="inquiry"/>
-              </Form.Group>
-            </Col>
-          </Form.Row>
-          <div className={styles.buttonContainer}>
-            <Button type="submit" variant="primary">Submit</Button>
-          </div>
-      </Container>
-    </Form>
-  </footer>
-);
+function encode(data) {
+  return Object.keys(data)
+    .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+    .join("&");
+}
 
-export default Footer;
+export default class Footer extends Component {
+  state = {
+    isSubmitted: false,
+  };
+
+  handleChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const form = e.target;
+    const { isSubmitted, ...formValues } = this.state;
+    fetch(form.action, {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({
+        "form-name": "contact",
+        ...formValues
+      })
+    })
+    .then(() => this.setState({ isSubmitted: true }))
+    .catch(error => console.error(error));
+  }
+
+  renderForm = () => (
+    <>
+      <input type="hidden" name="form-name" value="contact" />
+      <Form.Row>
+        <Col sm={12} md={6}>
+          <Form.Group controlId="firstName">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control type="text" name="first-name" onChange={this.handleChange} />
+          </Form.Group>
+        </Col>
+        <Col sm={12} md={6}>
+          <Form.Group controlId="lastName">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control type="text" name="last-name" onChange={this.handleChange} />
+          </Form.Group>
+        </Col>
+        <Col sm={12} md={6}>
+          <Form.Group controlId="emailAddress">
+            <Form.Label>Email Address</Form.Label>
+            <Form.Control type="email" name="email-address" onChange={this.handleChange} />
+          </Form.Group>
+        </Col>
+        <Col sm={12} md={6}>
+          <Form.Group controlId="phoneNumber">
+            <Form.Label>Phone Number</Form.Label>
+            <Form.Control type="tel" name="telephone-number" onChange={this.handleChange} />
+          </Form.Group>
+        </Col>
+        <Col sm={12} md={6}>
+          <Form.Group controlId="Inquiry">
+            <Form.Label>Inquiry</Form.Label>
+            <Form.Control as="textarea" rows="5" name="inquiry" onChange={this.handleChange} />
+          </Form.Group>
+        </Col>
+      </Form.Row>
+      <div className={styles.buttonContainer}>
+        <Button type="submit" variant="primary">Submit</Button>
+      </div>
+    </>
+  );
+
+  renderThankYou = () => (
+    <p>Thank you for inquiry! We will reach out as soon as we can.</p>
+  );
+
+  render() {
+    return (
+      <footer>
+        <form
+          name="contact"
+          method="POST"
+          data-netlify="true"
+          onSubmit={this.handleSubmit}
+          netlify-honeypot="bot-field"
+        >
+          <Container className={styles.form}>
+            <h1 className={styles.header}>Contact Us</h1>
+            { this.state.isSubmitted ? this.renderThankYou() : this.renderForm() }
+          </Container>
+        </form>
+      </footer>
+    );
+  }
+}
