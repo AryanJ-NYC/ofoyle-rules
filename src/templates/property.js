0ace8e789be9401a9d098ba2caef2c3d7cf4fa55
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2018-10-14 23:03:55 -0400

    Add gallery to property pages

--- src/templates/property.js
+++ src/templates/property.js
@@ -1,16 +1,24 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
+import { Container } from 'react-bootstrap';
+import ImageGallery from 'react-image-gallery';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import 'react-image-gallery/styles/css/image-gallery.css';
 import Content, { HTMLContent } from '../components/Content';
 import Layout from '../components/Layout';
 
-export const PropertyPageTemplate = ({ title, content, contentComponent }) => {
+export const PropertyPageTemplate = ({ images, title, content, contentComponent }) => {
   const PageContent = contentComponent || Content;
+  const imgItems = images.map(image => ({ original: image.imagePath, thumbnail: image.imagePath }));
   return (
     <>
-      <h1>{title}</h1>
-      <PageContent content={content} />
+      <ImageGallery items={imgItems} autoPlay={true} />
+      <Container>
+
+        <h1>{title}</h1>
+        <PageContent content={content} />
+      </Container>
     </>
   );
 }
@@ -28,6 +36,7 @@ const PropertyPage = ({ data }) => {
       <PropertyPageTemplate
         content={post.html}
         contentComponent={HTMLContent}
+        images={post.frontmatter.images}
         title={post.frontmatter.title}
       />
     </Layout>
@@ -42,6 +51,9 @@ export const indexPageQuery = graphql`
       html
       frontmatter {
         title
+        images {
+          imagePath
+        }
       }
     }
   }
